6084d91bb92475b09cf6fc089912277e2d38e1b5
jiayou1113 <jiayouz@naver.com>
2025-09-27 14:33:26 +0900

    quiz4

--- 유지아/Day2/quiz4.py
+++ 유지아/Day2/quiz4.py
@@ -7,9 +7,13 @@ for i in num:
     if i<2:
         continue
     else :
-        for j in range(2,i+1):
-            if j%i==0:
-                cnt+=1
+        k=1
+        for j in range(2,int(i**0.5) + 1):
+            if i%j==0:
+                k=0
+                break
+    if k==1:
+        cnt+=1
 print(cnt)
 
 
